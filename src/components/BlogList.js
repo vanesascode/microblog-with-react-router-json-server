500bc10fb35a23bb903b330b38fe73618deebdf8
vanesascode <vanesa.juarez.paris@gmail.com>
2023-06-15 18:15:06 +0200

    json data added

---
+++ src/components/BlogList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const BlogList = ({ blogs, title, handleDelete }) => {
+
+  //We don't create the function for the button here because we shouldn't edit the props directly. The data is initialized in the Home.js component. We need to use the method 'setBlogs' in Home.js to update the state. So, we create the function in Home.js and then we pass it here as a prop. 
+
+  return (
+    <div className='blog-list'>
+      <h2>{title}</h2>
+      {blogs.map((blog) => ( //Map Method has a callback function that returns a bit of jsx template. 'blog' is to refer to each item of the array state 'blogs' that is in 'Home.js'. onClick is an arrow function because we need to pass the argument id.
+        <div className='blog-preview' key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+        </div>
+      ))}
+
+    </div>
+  )
+}
+
+export default BlogList
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
 
 const Home = () => {
+
+  const [blogs, setBlogs] = useState(null) //after the first render, the state will be null! so***
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => (blog.id !== id));
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs') //promise
+      .then(res => { //response object, which we have to turn into JS
+        return res.json() //another promise
+      })
+      .then(data => {//we get the data
+        setBlogs(data) //and we set it as our state(blogs)
+      })
+  }, [])
+
+  //we want to pass the 'blogs' state into the child BlogList.js, so we do it through props. Remember 'blog' is 'item of blogs'. 
   return (
     <div className='home'>
-      <h2>Homepage</h2>
+      {blogs && <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />}
+      {blogs && <BlogList blogs={blogs.filter((blog) => (blog.author === 'mario'))} title="Mario's Blog" handleDelete={handleDelete} />}
     </div>
   )
 }
